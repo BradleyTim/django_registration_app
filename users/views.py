325feddb8bb70cd1eba4e8be95485db9de6cdd33
BradleyTim <timbradley94@gmail.com>
2020-07-31 20:01:09 +0300

    add mvp app

---
+++ users/views.py
@@ -0,0 +1,25 @@
+from django.contrib.auth import login
+from django.shortcuts import redirect, render
+from django.urls import reverse
+from users.forms import CustomUserCreationForm
+
+# users dashboard
+def dashboard(request):
+  return render(request, "users/dashboard.html")
+
+# register new user view
+def register(request):
+  if request.method == "GET":
+    return render(
+      request, "users/register.html",
+      {"form": CustomUserCreationForm}
+    )
+  elif request.method == "POST":
+    form = CustomUserCreationForm(request.POST)
+    if form.is_valid():
+      # user = form.save()
+      user = form.save(commit=False)
+      user.backend = "django.contrib.auth.backends.ModelBackend"
+      user.save()
+      login(request, user)
+      return redirect(reverse("dashboard"))
